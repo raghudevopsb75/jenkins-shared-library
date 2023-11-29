bc33c4ab97405acfacea2dbd21139a2a4c8a17a7
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-29 08:07:47 +0530

    [D75-7] | Raghu K | Terraform Code for RoboShop Project.

--- vars/cipipeline.groovy
+++ vars/cipipeline.groovy
@@ -2,12 +2,16 @@ def call() {
   node('workstation') {
     common.codeCheckout()
     common.compileCode()
-    stage('Code Quality') {
-      sh 'echo OK'
-    }
-    stage('Unit Tests') {
-      sh 'echo OK'
-    }
 
+    if(env.TAG_NAME ==~ ".*") {
+      common.appRelease()
+    } else {
+      stage('Code Quality') {
+        sh 'echo OK'
+      }
+      stage('Unit Tests') {
+        sh 'echo OK'
+      }
+    }
   }
 }

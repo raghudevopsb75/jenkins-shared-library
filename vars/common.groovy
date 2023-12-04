b081c0e8fba7fabf72593cb294e2f35a6ff24e6e
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-04 08:16:26 +0530

    [D75-7] | Raghu K | Terraform Code for RoboShop Project.

--- vars/common.groovy
+++ vars/common.groovy
@@ -25,22 +25,30 @@ def codeCheckout() {
   }
 }
 
+//def appRelease(appType) {
+//  sh 'echo ${TAG_NAME} >VERSION'
+//  stage('Release App Version') {
+//    if (appType == "nodejs") {
+//      sh 'zip -r ${component}-${TAG_NAME}.zip *'
+//    }
+//    if (appType == "java") {
+//      sh 'mv target/${component}-1.0.jar ${component}.jar'
+//      sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION schema'
+//    }
+//    if (appType == "python") {
+//      sh 'zip -r ${component}-${TAG_NAME}.zip *'
+//    }
+//    if (appType == "nginx") {
+//      sh 'zip -r ${component}-${TAG_NAME}.zip *'
+//    }
+//    sh 'curl -sSf -u "admin:Admin123" -X PUT -T ${component}-${TAG_NAME}.zip "http://artifactory.rdevopsb73.online:8081/artifactory/${component}/${component}-${TAG_NAME}.zip"'
+//  }
+//}
+
 def appRelease(appType) {
-  sh 'echo ${TAG_NAME} >VERSION'
-  stage('Release App Version') {
-    if (appType == "nodejs") {
-      sh 'zip -r ${component}-${TAG_NAME}.zip *'
-    }
-    if (appType == "java") {
-      sh 'mv target/${component}-1.0.jar ${component}.jar'
-      sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION schema'
-    }
-    if (appType == "python") {
-      sh 'zip -r ${component}-${TAG_NAME}.zip *'
-    }
-    if (appType == "nginx") {
-      sh 'zip -r ${component}-${TAG_NAME}.zip *'
-    }
-    sh 'curl -sSf -u "admin:Admin123" -X PUT -T ${component}-${TAG_NAME}.zip "http://artifactory.rdevopsb73.online:8081/artifactory/${component}/${component}-${TAG_NAME}.zip"'
+  stage('Docker Build') {
+    sh 'docker build -t 739561048503.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .'
+    sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 739561048503.dkr.ecr.us-east-1.amazonaws.com'
+    sh 'docker push 739561048503.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
   }
 }
